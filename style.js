e4d68c0611378ff9fb153d29b191822a1d57ee13
OladipoAnuoluwapo <oladipoanuoluwapo652@gmail.com>
2022-07-05 12:59:11 +0100

    first commit

---
+++ style.js
@@ -0,0 +1,36 @@
+const ntells= "50years"
+document.write(ntells)
+const sym= Symbol("mary");
+const sym2= Symbol("mary");
+
+console.log(sym === sym2)
+// const car= {
+//     color: "red",
+//     wheel: "four",
+//     model:"honda",
+//     year: 2005,
+//     new: false
+// }
+
+// console.log(car.model)
+// console.log(car)
+
+const car = {
+    color: "red",
+    wheel: "four",
+    model:"toyota",
+    year: 2005,
+    new: false,
+    seats: "three",
+    window: "tinted",
+    airconditioning: "good condition",
+    sunroof: "none",
+    overall: "good"
+}
+
+console.log(car.model)
+console.log(car)
+
+let car = {
+    color
+}
